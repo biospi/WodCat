8eae69570e7c11785882d5f362921a96c80c3de4
Axel Montout <axel.montout@bristol.ac.uk>
2024-01-04 12:34:05 +0000

    debug pandas warning.

--- model/data_loader.py
+++ model/data_loader.py
@@ -117,7 +117,8 @@ def load_activity_data(
 
     for i, flabel in enumerate(flabels):
         data_frame_labeled[flabel] = data_frame_labeled[flabel] * (i + 1)
-        data_frame.loc[:, "target"] = data_frame["target"] + data_frame_labeled[flabel]
+        #data_frame.loc[:, "target"] = data_frame["target"] + data_frame_labeled[flabel]
+        data_frame["target"] += data_frame_labeled[flabel]
 
     # store all samples for later testing after binary fitting
     labels = data_frame["label"].drop_duplicates().values
