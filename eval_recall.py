9ea52c9020953938086710cbee7204dd478fce0a
Axel Montout <axel.montout@bristol.ac.uk>
2024-07-05 16:08:44 +0100

    updated eval_recall.py

--- eval_recall.py
+++ eval_recall.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 import typer
 import json
@@ -61,66 +62,78 @@ def get_cli(data, l="Sensitivity"):
 def eval_recall(
     input_folder: Path = typer.Option(
         ..., exists=False, file_okay=False, dir_okay=True, resolve_path=True
-    )
+    ),
+    n_boots: int = 10
 ):
-    print("Find optimal Sensitivity/Specificity...")
-    print(input_folder)
-    fold_data_files = list(input_folder.glob("*.json"))
-    y_true_list = []
-    y_score_list = []
+    optimal_sensitivity_list, optimal_specificity_list = [], []
+    for i in range(n_boots):
+        print("Find optimal Sensitivity/Specificity...")
+        print(input_folder)
+        fold_data_files = list(input_folder.glob("*.json"))
 
-    optimal_sensitivity_list = []
-    optimal_specificity_list = []
+        num_files_to_select = int(len(fold_data_files) * 0.95)
+        selected_files = random.sample(fold_data_files, num_files_to_select)
 
-    for file in tqdm(fold_data_files):
-        with open(file, "r") as fp:
-            fold_data = json.load(fp)
-            y_true = fold_data["y_test"]
-            y_true_list.extend(y_true)
-            y_score = fold_data["y_pred_proba_test"]
-            y_score_list.extend(y_score)
+        y_true_list = []
+        y_score_list = []
 
-            print(y_true)
-            print(y_score)
-            fpr, tpr, thresholds = roc_curve(y_true, y_score)
-            sensitivity = tpr
-            specificity = 1 - fpr
-            optimal_idx = np.argmax(sensitivity + specificity)
-            optimal_threshold = thresholds[optimal_idx]
-            optimal_sensitivity = sensitivity[optimal_idx]
-            optimal_specificity = specificity[optimal_idx]
-            optimal_sensitivity_list.append(optimal_sensitivity)
-            optimal_specificity_list.append(optimal_specificity)
+        # optimal_sensitivity_list = []
+        # optimal_specificity_list = []
+
+        for file in tqdm(selected_files):
+            with open(file, "r") as fp:
+                fold_data = json.load(fp)
+                y_true = fold_data["y_test"]
+                y_true_list.extend(y_true)
+                y_score = fold_data["y_pred_proba_test"]
+                y_score_list.extend(y_score)
+
+        #         print(y_true)
+        #         print(y_score)
+        #         fpr, tpr, thresholds = roc_curve(y_true, y_score)
+        #         sensitivity = tpr
+        #         specificity = 1 - fpr
+        #         optimal_idx = np.argmax(sensitivity + specificity)
+        #         optimal_threshold = thresholds[optimal_idx]
+        #         optimal_sensitivity = sensitivity[optimal_idx]
+        #         optimal_specificity = specificity[optimal_idx]
+        #         optimal_sensitivity_list.append(optimal_sensitivity)
+        #         optimal_specificity_list.append(optimal_specificity)
+        #
+        # get_cli(optimal_sensitivity_list, "optimal_sensitivity")
+        # get_cli(optimal_specificity_list, "optimal_specificity")
+
+        fpr, tpr, thresholds = roc_curve(y_true_list, y_score_list)
+        sensitivity = tpr
+        specificity = 1 - fpr
+
+        # Find the index of the point on the ROC curve closest to the top-left corner
+        optimal_idx = np.argmax(sensitivity + specificity)
+
+        optimal_threshold = thresholds[optimal_idx]
+        optimal_sensitivity = sensitivity[optimal_idx]
+        optimal_specificity = specificity[optimal_idx]
+
+        print("Optimal Threshold:", optimal_threshold)
+        print("Optimal Sensitivity:", optimal_sensitivity)
+        print("Optimal Specificity:", optimal_specificity)
+
+        auc = roc_auc_score(y_true_list, y_score_list)
+        print("AUC:", auc)
+        df = pd.DataFrame()
+        df["auc"] = [auc]
+        df["optimal_threshold"] = [optimal_threshold]
+        df["optimal_sensitivity"] = [optimal_sensitivity]
+        df["optimal_specificity"] = [optimal_specificity]
+        filepath = input_folder.parent / "recall_data.csv"
+        print(filepath)
+        df.to_csv(filepath, index=False)
+        optimal_sensitivity_list.append(optimal_sensitivity)
+        optimal_specificity_list.append(optimal_specificity)
+        #return auc, optimal_threshold, optimal_sensitivity, optimal_specificity
 
     get_cli(optimal_sensitivity_list, "optimal_sensitivity")
     get_cli(optimal_specificity_list, "optimal_specificity")
-
-    fpr, tpr, thresholds = roc_curve(y_true_list, y_score_list)
-    sensitivity = tpr
-    specificity = 1 - fpr
-
-    # Find the index of the point on the ROC curve closest to the top-left corner
-    optimal_idx = np.argmax(sensitivity + specificity)
-
-    optimal_threshold = thresholds[optimal_idx]
-    optimal_sensitivity = sensitivity[optimal_idx]
-    optimal_specificity = specificity[optimal_idx]
-
-    print("Optimal Threshold:", optimal_threshold)
-    print("Optimal Sensitivity:", optimal_sensitivity)
-    print("Optimal Specificity:", optimal_specificity)
-
-    auc = roc_auc_score(y_true_list, y_score_list)
-    print("AUC:", auc)
-    df = pd.DataFrame()
-    df["auc"] = [auc]
-    df["optimal_threshold"] = [optimal_threshold]
-    df["optimal_sensitivity"] = [optimal_sensitivity]
-    df["optimal_specificity"] = [optimal_specificity]
-    filepath = input_folder.parent / "recall_data.csv"
-    print(filepath)
-    df.to_csv(filepath, index=False)
-    return auc, optimal_threshold, optimal_sensitivity, optimal_specificity
 
 
 if __name__ == "__main__":
