2ea5ee9bd69ee7a6bee50e3e5568c69d0adf4da8
Axel Montout <axel.montout@bristol.ac.uk>
2024-10-27 10:42:12 +0000

    updated loop for all clf.

--- paper.py
+++ paper.py
@@ -97,7 +97,7 @@ def main(
                 build_dataset.run(
                     w_size=[30],
                     threshs=[25],
-                    n_peaks=[1],
+                    n_peaks=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                     data_dir=data_dir,
                     out_dir=out_dir,
                     max_sample=max_sample,
